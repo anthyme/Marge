687f5727a4fd89ca796fcb74ba43f63b91ec2b21
anthyme <anthyme@gmail.com>
2017-02-23 14:30:13 +0100

    Add InMemoryEventStore

--- Marge.Infrastructure/MemoryEventStore.cs
+++ Marge.Infrastructure/MemoryEventStore.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Marge.Common;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Marge.Infrastructure
 {
@@ -10,4 +11,18 @@ namespace Marge.Infrastructure
         void Save(EventWrapper evt);
     }
 
+    public class InMemoryEventStore : IEventStore
+    {
+        private readonly List<EventWrapper> events = new List<EventWrapper>();
+
+        public IEnumerable<EventWrapper> RetrieveAllEvents(Guid id)
+        {
+            return events.Where(x => x.StreamId == id).ToList();
+        }
+
+        public void Save(EventWrapper evt)
+        {
+            events.Add(evt);
+        }
+    }
 }
---
+++ Marge.Tests/Infrastructure/InMemoryEventStoreTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using Marge.Common;
+using Marge.Common.Events;
+using Marge.Infrastructure;
+using NFluent;
+using Xunit;
+
+namespace Marge.Tests.Infrastructure
+{
+    public class InMemoryEventStoreTest
+    {
+        private readonly InMemoryEventStore sut;
+
+        public InMemoryEventStoreTest()
+        {
+            sut = new InMemoryEventStore();
+        }
+
+        [Fact]
+        public void WhenSavingEventThenTheyCanBeRetrieved()
+        {
+            var id1 = Guid.NewGuid();
+            var id2 = Guid.NewGuid();
+
+            var events = new[]
+            {
+                new EventWrapper(id1, new PriceCreated(10, 10, 10, 10)),
+                new EventWrapper(id1, new DiscountChanged(10, 10, 10)),
+                new EventWrapper(id2, new PriceCreated(20, 10, 10, 10)),
+                new EventWrapper(id1, new DiscountChanged(10, 5, 1)),
+                new EventWrapper(id2, new DiscountChanged(20, 5, 1)),
+                new EventWrapper(id1, new DiscountChanged(10, 5, 1)),
+                new EventWrapper(id1, new DiscountChanged(10, 20, 30)),
+            };
+
+
+            var expected = new[]
+            {
+                new EventWrapper(id2, new PriceCreated(20, 10, 10, 10)),
+                new EventWrapper(id2, new DiscountChanged(20, 5, 1)),
+            };
+
+            events.ToList().ForEach(sut.Save);
+
+            Check.That(sut.RetrieveAllEvents(id2)).ContainsExactly(expected);
+        }
+    }
+}
